2d3f9ac2ceb6df415c094080cc62ec501c51d2cc
Alexander Samacá <alexadrito10@gmail.com>
2020-10-11 12:54:23 -0500

    Calculo Velocidad Inicial

--- app/src/main/java/com/feraldragons/physicscalculator/Calculator.java
+++ app/src/main/java/com/feraldragons/physicscalculator/Calculator.java
@@ -4,14 +4,6 @@ import android.text.PrecomputedText;
 
 public class Calculator {
 
-    public double[] calculateMasses(String initialVelocity1, String initialVelocity2,String finalVelocity1, String finalVelocity2 ){
-
-        return null;
-    }
-    public double[] calculateInitialVelocities(String mass1, String mass2,String finalVelocity1, String finalVelocity2 ){
-
-        return null;
-    }
 
     public double[]calculateFinalVelocities(double mass1, double mass2, double initialVelocity1, double initialVelocity2){
         double results[] = new double[2];
@@ -27,7 +19,24 @@ public class Calculator {
     }
     private double calculateVFinal2(double mass1,double mass2,double initialVelocity1,double initialVelocity2,double finalVelocity1){
         double result= 0;
-        result = (mass1/mass2)/(initialVelocity1-finalVelocity1)+initialVelocity2;
+        result = ((mass1/mass2)*(initialVelocity1-finalVelocity1))+initialVelocity2;
+        return result;
+    }
+    public double[]calculateInitialVelocities(double mass1, double mass2, double finalVelocity1, double finalVelocity2){
+        double results[] = new double[2];
+        results[0]= calculateVInitial1(mass1,mass2,finalVelocity1,finalVelocity2);
+        results[1]= calculateVInitial2(mass1,mass2,finalVelocity1,finalVelocity2,results[0]);
+
+        return results;
+    }
+    private double calculateVInitial1(double mass1,double mass2,double finalVelocity1, double finalVelocity2){
+        double result= 0;
+        result = ((mass1-mass2)/(mass1+mass2))*finalVelocity1 + ((2*mass2)/(mass1+mass2))*finalVelocity2;
+        return result;
+    }
+    private double calculateVInitial2(double mass1,double mass2,double finalVelocity1, double finalVelocity2,double initialVelocity1){
+        double result= 0;
+        result = finalVelocity2+((mass1/mass2)*(initialVelocity1-finalVelocity1));
         return result;
     }
 
